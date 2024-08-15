7d6bbe38433dd6e5a7c1910e47c754be3bf8c324
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-15 16:59:05 +0330

    add uesr_data

--- code/01-why-terraform/main.tf
+++ code/01-why-terraform/main.tf
@@ -2,6 +2,14 @@ resource "aws_instance" "example" {
   ami           = "ami-03e31863b8e1f70a5"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.xhtml
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "dev-ec2"
   }
